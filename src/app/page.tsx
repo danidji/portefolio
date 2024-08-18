f8220bcedcd86d739ad74f0a45aa34713255407f
danGbe <73373221+danidji@users.noreply.github.com>
2024-08-18 16:31:20 +0200

    style: add font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <main className=''></main>
+  return (
+    <main className=''>
+      <h1>Hello</h1>
+      <h2>Hello</h2>
+      <h3>Hello</h3>
+      <h4>Hello</h4>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit
+        laborum cum totam molestiae rerum dolorem repellat odit, itaque numquam
+        ex ad cumque quaerat debitis quisquam ratione aut minus unde! Dolores
+        nam nisi animi possimus itaque magni quod rerum saepe sequi. Cum
+        molestiae possimus accusamus autem deleniti iure, aliquid voluptas
+        excepturi.
+      </p>
+    </main>
+  )
 }

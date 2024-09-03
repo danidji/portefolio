7fbd8f6ede73eb9959eb4b960b14317b5bc60f21
danGbe <73373221+danidji@users.noreply.github.com>
2024-09-03 22:56:26 +0200

    feat: create update user action

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -1,6 +1,7 @@
 'use server'
 import {auth} from '@/auth'
 import prisma from '@/lib/prisma'
+import {TUser} from '@/models'
 
 export async function getUserDetails() {
   const session = await auth()
@@ -22,4 +23,29 @@ export async function getUserDetails() {
   const {password, ...userWithoutPassword} = user
 
   return userWithoutPassword
+}
+
+export async function updateUserDetails(data: Partial<TUser>) {
+  const session = await auth()
+
+  if (!session?.user?.email) {
+    throw new Error('User not authenticated')
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  })
+
+  if (!user) {
+    throw new Error('User not found')
+  }
+
+  return prisma.user.update({
+    where: {
+      email: session.user.email,
+    },
+    data,
+  })
 }
